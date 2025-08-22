b38bd032dc4a70e5be38d2ca47e149b0fc6c3df1
João Victor dos Santos Coutinho <joao.v.coutinho7@ba.estudante.senai.br>
2025-08-22 16:58:18 -0300

    Add files via upload

---
+++ 7.tudo_junto_.py
@@ -0,0 +1,21 @@
+
+
+
+valor_1 =int(input("digite o primeiro valor: ")) 
+valor_2 = int(input("digite o degundo valor: "))  
+
+soma = valor_1 + valor_2
+produto = valor_1 * valor_2
+media = soma / 2
+
+
+maior = max(valor_1, valor_2)
+menor = min(valor_1, valor_2)
+
+5
+    
+print(f"soma: {soma}")
+print(f"produto: {produto}")
+print(f"média: {media}")
+print(f"maior número: {maior}")
+print(f"menor número: {menor}")
