cbf73749a07110bb763f279dec682a962ad0e150
João Victor dos Santos Coutinho <joao.v.coutinho7@ba.estudante.senai.br>
2025-08-29 16:53:16 -0300

    Add files via upload

---
+++ 1.operacoes_logicas copy.py
@@ -0,0 +1,15 @@
+
+import os
+os.system("cls")
+
+nota = 10
+
+# se nota é maior ou igual a zero e
+# nota menor ou igual a zero
+#or fora do intervalo definido
+if nota <0 or nota > 10:
+    print("nota inválida.")
+else:
+    print(f"nota: {nota}")    
+
+
